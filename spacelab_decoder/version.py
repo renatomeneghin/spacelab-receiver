8d07d27a3734c63b264751e8d67d7c88ca42656e
Renato Augusto Schenkel Meneghin Marchiori <renato.meneghin@posgrad.ufsc.br>
2024-05-20 23:38:21 -0300

    versioning

--- spacelab_decoder/version.py
+++ spacelab_decoder/version.py
@@ -1,30 +1,30 @@
 #
 #  version.py
 #  
-#  Copyright The SpaceLab-Decoder Contributors.
+#  Copyright The SpaceLab-Receiver Contributors.
 #  
-#  This file is part of SpaceLab-Decoder.
+#  This file is part of SpaceLab-Receivervv.
 #
-#  SpaceLab-Decoder is free software; you can redistribute it
+#  SpaceLab-Receiver is free software; you can redistribute it
 #  and/or modify it under the terms of the GNU General Public License as
 #  published by the Free Software Foundation, either version 3 of the
 #  License, or (at your option) any later version.
 #  
-#  SpaceLab-Decoder is distributed in the hope that it will be useful,
+#  SpaceLab-Receiver is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
 #  
 #  You should have received a copy of the GNU General Public
-#  License along with SpaceLab-Decoder; if not, see <http://www.gnu.org/licenses/>.
+#  License along with SpaceLab-Receiver; if not, see <http://www.gnu.org/licenses/>.
 #  
 #
 
-__author__      = "Gabriel Mariano Marcelino - PU5GMA"
-__copyright__   = "Copyright The SpaceLab-Decoder Contributors"
-__credits__     = ["Gabriel Mariano Marcelino - PU5GMA"]
+__author__      = "Renato Meneghin - Modified from: Gabriel Mariano Marcelino - PU5GMA"
+__copyright__   = "Copyright The SpaceLab-Receiver Contributors"
+__credits__     = ["Renato Meneghin - Modified from: Gabriel Mariano Marcelino - PU5GMA"]
 __license__     = "GPLv3"
-__version__     = "0.3.19"
-__maintainer__  = "Gabriel Mariano Marcelino - PU5GMA"
-__email__       = "gabriel.mm8@gmail.com"
+__version__     = "0.1.0"
+__maintainer__  = "Renato Meneghin"
+__email__       = "renato.meneghin@posgrad.ufsc.br"
 __status__      = "Development"
